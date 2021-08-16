32194b506b9d90e240217f9108f0121c450474b2
Md Nazmul Hasan <farhapartex@gmail.com>
2021-08-17 03:05:57 +0600

    store all models added

---
+++ store/enums/product.py
@@ -0,0 +1,2 @@
+from django.utils.translation import gettext_lazy as _
+from django.db import models
--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from inventory.models import BaseEntity, BaseAbstractEntity
+from core.models import User
 # Create your models here.
+
+
+class Store(BaseEntity):
+    owner = models.OneToOneField(User, related_name="store", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(BaseEntity):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseEntity):
+    store = models.ForeignKey(Store, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(ProductCategory, related_name="products", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
